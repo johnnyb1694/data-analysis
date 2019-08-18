e5ed4e0b1155ceb5f97a3cab6518680cc2ffd3f8
johnnyb1694 <johnnyb1694@gmail.com>
2019-08-18 16:31:09 +0100

    Updated script to handle BC / AD birth and death periods respectively!

--- Analysis of Roman emperors/roman-emperor-analysis.R
+++ Analysis of Roman emperors/roman-emperor-analysis.R
@@ -4,7 +4,7 @@
 
 ## Input parameters ##
 
-required_packages <- c("tidyverse", "ggsci", "extrafont")
+required_packages <- c("tidyverse", "ggsci", "extrafont", "lubridate")
 data_url <- "https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-08-13/emperors.csv"
 
 ## Load packages ##
@@ -24,5 +24,17 @@ emp_raw <- readr::read_csv(file = data_url)
 
 #### Clean data ####
 
+# 1. Don't think we need 'name_full' (as 'name' suffices), 'notes' or 'verif_who'
+# 2. We can engineer new features: 'lifespan' and 'length_of_reign'
+
+emp_clean <- emp_raw %>%
+  select(-c("name_full", "notes", "verif_who")) %>%
+  mutate(birth_period = ifelse(death < birth, "BC", "AD"),
+         reign_period = ifelse(reign_end < reign_start, "BC", "AD"),
+         lifespan = ifelse(birth_period == "BC", 
+                           (birth - ymd("0000-01-01")) + (death - ymd("0000-01-01")),
+                           (death - birth)) / 365.25)
+
 #### Explore data ####
 
+# 1. What 
