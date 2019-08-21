d545024f588294ab25b9f830c4b86d4eab320541
johnnyb1694 <johnnyb1694@gmail.com>
2019-08-21 21:50:23 +0100

    Added a t-test and a small count plot.

--- Analysis of Roman emperors/roman-emperor-analysis.R
+++ Analysis of Roman emperors/roman-emperor-analysis.R
@@ -31,10 +31,32 @@ emp_clean <- emp_raw %>%
   select(-c("name_full", "notes", "verif_who")) %>%
   mutate(birth_period = ifelse(death < birth, "BC", "AD"),
          reign_period = ifelse(reign_end < reign_start, "BC", "AD"),
-         lifespan = ifelse(birth_period == "BC", 
+         length_of_life = ifelse(birth_period == "BC", 
                            (birth - ymd("0000-01-01")) + (death - ymd("0000-01-01")),
-                           (death - birth)) / 365.25)
+                           (death - birth)) / 365.25,
+         length_of_reign = ifelse(reign_period == "BC",(reign_start - ymd("0000-01-01")) + (reign_end - ymd("0000-01-01")),
+                                  (reign_end - reign_start)) / 365.25,
+         avg_reign = reign_start + length_of_reign*365.25 / 2)
 
 #### Explore data ####
 
-# 1. What 
+extrafont::loadfonts()
+
+theme_set(theme_light(base_size = 9, base_family = "Arial"))
+
+emp_clean %>%
+  group_by(killer) %>%
+  summarise(avg_reign = mean(length_of_reign, na.rm = TRUE)) %>%
+  ungroup() %>%
+  ggplot() +
+  geom_point(aes(x = avg_reign, y = killer))
+
+emp_clean %>%
+  nest(-cause) %>%
+  mutate(n_obs = map_int(data, ~ nrow(.))) %>%
+  filter(n_obs > 1) %>%
+  mutate(t_test = map(data, ~ t.test(.$length_of_reign))) %>%
+  mutate(t_test_tidy = map(t_test, ~ broom::tidy(.))) %>%
+  unnest(t_test_tidy)
+
+
