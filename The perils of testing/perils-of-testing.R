64c56dc9851f641dc3bf239a057e23c855694de8
johnnyb1694 <johnnyb1694@gmail.com>
2020-03-28 17:29:04 +0000

    Initial commit of analysis concerned with Bayesian inference in medical testing.

---
+++ The perils of testing/perils-of-testing.R
@@ -0,0 +1,24 @@
+#### Why 'testing' requires 'testing' ####
+
+# The problem ----
+
+# You've been feeling under the weather for more than a couple of days and things don't seem to be improving. 
+# So, you pluck up the courage to visit your local doctor. He tells you that it's probably just an innocuous
+# virus but he decides to run a series of tests on you regardless, 'just to be safe'. 
+
+# The next day you receive your results: your doctor informs you that you have tested positive
+# for a very rare disease that has a life expectancy of approximately 10 years from diagnosis.
+
+# "The test", your doctor says, "is 99% accurate."
+
+# You fall to your knees: "99% accurate? My life is over."
+
+# ... Or is it?
+
+# Preliminaries ----
+req_pkgs <- c("tidyverse")
+
+# 
+
+
+
