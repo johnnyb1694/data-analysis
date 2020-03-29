b07ce0d9314a93915bcc4b35c4edd3d23d1f2a97
johnnyb1694 <johnnyb1694@gmail.com>
2020-03-29 10:18:30 +0100

    Updated font on plots.

--- The perils of testing/perils-of-testing.R
+++ The perils of testing/perils-of-testing.R
@@ -7,13 +7,13 @@
 # virus but he decides to run a series of tests on you regardless, 'just to be safe'. 
 
 # The next day you receive your results: your doctor informs you that you have tested positive
-# for a very serious disease that affects only 5% of the global population.
+# for a very serious disease that only affects approximately 5% of the global population.
 
-# "The test", your doctor informs you, "is 99% accurate and it's only wrong 5 times out of 100 on otherwise healthy patients."
+# "The test", your doctor informs you, "is 99% accurate. Statistics show that it's only wrong 5 times out of 100 on otherwise healthy patients."
 
 # You fall to your knees: "99% accurate? It's got to be right, surely?"
 
-# ... Or is it?
+# Or is it?
 
 # Preliminaries ----
 req_pkgs <- c("tidyverse", "scales")
@@ -25,7 +25,8 @@ for (pkg in req_pkgs) {
   library(pkg, character.only = T)
 }
 
-theme_set(theme_light())
+extrafont::loadfonts()
+theme_set(theme_light(base_size = 12, base_family = "Arial"))
 
 # Input parameters ----
 
@@ -48,11 +49,11 @@ compute_posterior(p_A = disease_coverage, p_BA = test_accuracy, p_BAc = false_po
 
 # Imagine that our population consists of 10,000 individuals. This means that there are 500 individuals carrying 
 # the fictitious disease in our population. We apply our test to the entire population.
-population_size <- 1000
+population_size <- 10000
 
-v <- rep(10, 100)
+v <- rep(55, 100)
 
-for (s in seq(20, 100, 10)) {
+for (s in seq(100, 60, -5)) {
   # Don't copy this practice! It's a short-term fix
   v <- c(v, rep(s, 100))
 }
@@ -60,29 +61,53 @@ for (s in seq(20, 100, 10)) {
 population_data <- tibble(individual = 1:population_size, 
                           actual_status = c(rep("Carrier", 0.05*population_size), rep("Healthy", 0.95 * population_size)),
                           x = rep(seq(1, 100), 10),
-                          y = v,
-                          class = )
+                          y = v)
 
 population_data %>%
   ggplot(aes(x, y)) +
-  geom_point(aes(colour = actual_status)) +
+  geom_point(aes(colour = actual_status), shape = 1, size = 2.0) +
   theme_void() +
-  labs(title = "Our population",
+  labs(title = "Our population*",
        subtitle = "The red dots represent the actual carriers of the disease",
-       colour = "Actual status")
+       caption = "*population visualisation is restricted to 1,000 individuals for illustrative purposes",
+       colour = "Actual status") +
+  theme(plot.title = element_text(size = 15, face = "bold"),
+        plot.caption = element_text(size = 8, colour = "gray50"),
+        panel.grid = element_blank())
 
 # Given the parameters of the test, we end up with 495 individuals being correctly identified as carrying the virus.
 # In addition, we end up falsely identifying 475 individuals as positive. 
 population_data %>%
+  mutate(test_result = c(rep("Positive", 49), "Negative", rep("Positive", 48), rep("Negative", 902))) %>%
   ggplot(aes(x, y)) +
-  geom_point(aes(colour = actual_status)) +
-  theme_void() +
-  labs(title = "Our population",
-       subtitle = "The red dots represent the actual carriers of the disease",
-       colour = "Actual status")
+  geom_point(aes(colour = actual_status, shape = test_result), size = 2.0) +
+  theme_void() + 
+  scale_shape_manual(values = c(1, 7)) +
+  labs(title = "Our population*: test results",
+       subtitle = "Notice how we have incorrectly classified a relatively large number of healthy cases as positive",
+       caption = "*population visualisation is restricted to 1,000 individuals for illustrative purposes",
+       colour = "Actual status",
+       shape = "Test result") +
+  theme(plot.title = element_text(size = 15, face = "bold"),
+        plot.caption = element_text(size = 8, colour = "gray50"),
+        panel.grid = element_blank())
 
 # As a result, we end up with 970 individuals who have all tested positive, yet only 495 of these individuals are
 # indeed positive. So, actually only 51.03% of those who tested positive are *actually* positive.
+population_data %>%
+  mutate(test_result = c(rep("Positive", 49), "Negative", rep("Positive", 48), rep("Negative", 902))) %>%
+  filter(test_result == "Positive") %>%
+  ggplot(aes(x, y)) +
+  geom_point(aes(colour = actual_status), shape = 7, size = 2.0) +
+  theme_void() +
+  scale_alpha(guide = 'none') +
+  labs(title = "Our population*: 'positive' cases",
+       subtitle = "Notice how only around half of these 'positive' cases are truly positive",
+       caption = "*population visualisation is restricted to 1,000 individuals for illustrative purposes",
+       colour = "Actual status") + 
+  theme(plot.title = element_text(size = 15, face = "bold"),
+        plot.caption = element_text(size = 8, colour = "gray50"),
+        panel.grid = element_blank())
 
 # This particular example just demonstrates how poorly human beings are able to interpret probabilities.
 
@@ -101,8 +126,8 @@ sensitivity_fp_rate %>%
   mutate(prob_misdiagnosis = 1 - posterior_prob) %>%
   ggplot(aes(x = false_positive_sequence,
              y = prob_misdiagnosis)) +
-  geom_point(aes(colour = prob_misdiagnosis, size = prob_misdiagnosis), show.legend = F) +
-  geom_line(alpha = 0.75, show.legend = F) +
+  geom_point(aes(colour = 1 - prob_misdiagnosis, size = prob_misdiagnosis), show.legend = F) +
+  geom_line(alpha = 0.25, show.legend = F) +
   scale_colour_continuous(low = "tomato2", high = "slateblue1") +
   scale_y_continuous(labels = scales::percent_format(),
                      breaks = seq(0, 1.0, 0.20)) +
@@ -112,7 +137,10 @@ sensitivity_fp_rate %>%
   labs(x = "False positive rate",
        y = "Probability of misdiagnosing healthy case",
        title = "Probability of misdiagnosis versus false positive rate",
-       subtitle = "Tests with a low specificity tend to misdiagnose many healthy cases")
+       subtitle = "Tests with a low specificity tend to misdiagnose many healthy cases") + 
+  theme(plot.title = element_text(size = 15, face = "bold"),
+        plot.caption = element_text(size = 8, colour = "gray50"),
+        panel.grid = element_blank())
 
 # However, another important factor is how prevalent the underlying disease is in the population.
 # The more common it is, the less chance we have of identifying false positive cases.
@@ -125,8 +153,8 @@ sensitivity_coverage %>%
   mutate(prob_misdiagnosis = 1 - posterior_prob) %>%
   ggplot(aes(x = coverage_sequence,
              y = prob_misdiagnosis)) +
-  geom_point(aes(colour = prob_misdiagnosis, size = prob_misdiagnosis), show.legend = F) +
-  geom_line(alpha = 0.75, show.legend = F) +
+  geom_point(aes(colour = 1 - prob_misdiagnosis, size = prob_misdiagnosis), show.legend = F) +
+  geom_line(alpha = 0.25, show.legend = F) +
   scale_colour_continuous(low = "tomato2", high = "slateblue1") +
   scale_y_continuous(labels = scales::percent_format(),
                      breaks = seq(0, 1.0, 0.20)) +
@@ -136,4 +164,7 @@ sensitivity_coverage %>%
   labs(x = "Prevalence of disease (amongst population)",
        y = "Probability of misdiagnosing healthy case",
        title = "Probability of misdiagnosis versus disease prevalence",
-       subtitle = "Tests aimed at identifying common diseases are generally more reliable")
+       subtitle = "Tests aimed at identifying common diseases are generally more easy to rely upon") + 
+  theme(plot.title = element_text(size = 15, face = "bold"),
+        plot.caption = element_text(size = 8, colour = "gray50"),
+        panel.grid = element_blank())
