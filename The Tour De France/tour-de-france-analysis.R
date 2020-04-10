a928da6a58e25fb88a964ec69ab364485f2b2f10
johnnyb1694 <johnnyb1694@gmail.com>
2020-04-10 13:46:38 +0100

    Initial commit of 'Tour De France' analysis

---
+++ The Tour De France/tour-de-france-analysis.R
@@ -0,0 +1,9 @@
+#### An analysis of data on the Tour De France ####
+
+## Author: Johnny Breen ##
+
+#### Background ----
+
+# The data for this script comes from Alastair Rushworth's brilliant 'tdf' package. All credit goes to Alastair for making this data available.
+
+# The details on how to source this data can be found here: https://github.com/alastairrushworth/tdf
