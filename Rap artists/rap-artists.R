1016abc2715f4bb9fc19fc85ca84c386e43b7a67
johnnyb1694 <johnnyb1694@gmail.com>
2020-04-18 12:05:59 +0100

    Final commit of rap artists

--- Rap artists/rap-artists.R
+++ Rap artists/rap-artists.R
@@ -7,39 +7,16 @@ library(tidyverse)
 library(broom)
 library(png)
 library(grid)
+library(extrafont)
+library(ggrepel)
 
 polls <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-14/polls.csv')
 rankings <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-04-14/rankings.csv')
 
-# There's always been more males involved in rap from the get-go
-rankings %>% 
-  count(year, gender) %>% 
-  arrange(year) %>% 
-  ggplot(aes(x = year, y = n, colour = gender)) + 
-  geom_line()
-
-# It seems like there's slighly more love from US critics for the 90s than any other period of time
-europe <- c("UK", "Germany")
-asia <- c("Japan", "India", "China", "New Zealand")
-americas <- c("US", "Canada", "Colombia")
-africa <- c("South Africa", "Nigeria", "Kenya")
-russia <- c("Russian Federation")
-
-polls %>% 
-  mutate(critic_country_cat = case_when(critic_country %in% europe ~ "Europe",
-                                        critic_country %in% asia ~ "Asia",
-                                        critic_country %in% americas ~ "Americas",
-                                        critic_country %in% africa ~ "Africa",
-                                        critic_country %in% russia ~ "Russia")) %>%
-  ggplot(aes(y = year, x = critic_country_cat, colour = critic_country_cat)) +
-  geom_jitter(alpha = 0.50, width = 0.20) +
-  coord_flip()
-
 # Certain artists are good 'bang for their buck' - for instance, 'The Pharcyde' and 'Souls of Mischief'
-png_raw <- readPNG("/Users/Johnny/Desktop/Data Science/R/TidyTuesdays/PNGs/rakim-png2.png")
-png_clean <- rasterGrob(png_raw, interpolate = T)
+extrafont::loadfonts()
 
-rankings %>%
+top_10 <- rankings %>%
   group_by(artist) %>%
   summarise(total_points = sum(points),
             total_votes = sum(n),
@@ -48,19 +25,40 @@ rankings %>%
   filter(total_votes > 5) %>%
   arrange(desc(points_per_vote)) %>%
   head(10) %>%
-  mutate(artist = fct_reorder(artist, points_per_vote)) %>%
-  ggplot(aes(x = artist, y = points_per_vote, size = total_tracks)) +
-  annotation_custom(grob = png_clean, xmin = 6, xmax = 8, ymin = 6, ymax = 6.50) +
-  geom_point(colour = "orange") +
+  mutate(artist = fct_reorder(artist, points_per_vote)) 
+
+one_hit_titles <- one_hit_wonders %>% inner_join(rankings, by = "artist") %>% select(artist, title, points_per_vote)
+
+ggplot(top_10, aes(x = artist, y = points_per_vote)) +
+  geom_segment(data = filter(top_10, total_tracks != 1), colour = "orange", mapping = aes(x = artist, xend = artist,
+                                                 y = 6, yend = points_per_vote),
+               lty = 5,
+               alpha = 0.50) +
+  geom_segment(data = filter(top_10, total_tracks == 1), colour = "tomato2", mapping = aes(x = artist, xend = artist,
+                                                                                         y = 6, yend = points_per_vote),
+               lty = 1,
+               alpha = 1) +
+  geom_point(mapping = aes(size = total_tracks, colour = (total_tracks == 1)), show.legend = F) +
+  geom_label_repel(data = one_hit_titles, mapping = aes(label = title), size = 3, fill = "tomato2", colour = "white") +
+  scale_colour_manual(values = c("orange", "tomato2")) +
   coord_flip() +
   theme_light() +
   scale_size(guide = "none") +
-  labs(x = NULL,
+  labs(caption = "Source: BBC Music",
+       x = NULL,
        y = "Average points per vote",
-       title = "Top 10 rap artists of all time",
-       subtitle = "The size of each point corresponds to the number of tracks registered by each artist") +
+       title = "Top 10: Critics' choice",
+       subtitle = "The size of each point corresponds to the number of tracks for each artist.\nLegendary one-hit classics are shown in red.") +
   theme(panel.grid = element_blank(),
-        plot.title = element_text(face = "bold"))
+        plot.title = element_text(face = "bold", size = 12),
+        plot.subtitle = element_text(size = 9),
+        plot.background = element_rect(fill = "gray10"),
+        panel.background = element_rect(fill = "gray40"),
+        text = element_text(colour = "white", family = "Verdana"),
+        axis.text = element_text(colour = "white", size = 8.5),
+        axis.title.x = element_text(face = "bold"),
+        plot.caption = element_text(colour = "gray60")
+        )
   
   
   
